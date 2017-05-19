3ed2fb7cd6a4ebbd648cd8235f62d1863f8e1cfa
Waterstrong <sqlin@thoughtworks.com>
2017-05-19 22:38:05 +0800

    add confirm compression process

--- README.md
+++ README.md
@@ -12,12 +12,13 @@ cd tiny-png
 
 ## Usage
 
-After clone `tiny-png`, change `tinify.key.template` to `tinify.key` and paste your **tinify key** in it. Then start to compress the images in `target_directory`:
+- After clone `tiny-png`, change `tinify.key.template` to `tinify.key`
+- Paste your **tinify key** in it. Then start to compress the images in `target_directory`
 ```
 python compress.py <target_directory>
 ```
 
-**IMPORTANT:** This will replace all the `*.jpg|*.png` images in that target directory.
+**IMPORTANT:** The script will replace all the `*.jpg|*.png` images in the target directory.
 
 ## Powered By:
 - [https://tinypng.com/](https://tinypng.com/)
--- compress.py
+++ compress.py
@@ -3,33 +3,48 @@ import sys
 import re
 import tinify
 
-tinify_key_file = open('tinify.key', 'r')
-tinify.key = tinify_key_file.read()
-tinify_key_file.close()
-
 # TODO: support more image types?
 image_pattern = re.compile('.*\.(jpg$|png$)')
 
+def initializeTinifyKey():
+    print 'Initializing tinify key...'
+    tinify_key_file = open('tinify.key', 'r')
+    tinify.key = tinify_key_file.read()
+    tinify_key_file.close()
 
-def Scanning(rootDir):
+
+def scanning(rootDir):
     for file in os.listdir(rootDir):
         path = os.path.join(rootDir, file)
         if os.path.isdir(path):
-            Scanning(path)
+            scanning(path)
         elif image_pattern.match(path):
-            print 'Start to compress image:', path
+            print 'Start compressing image:', path
             tinify.from_file(path).to_file(path)
             print 'Compression done!\n'
 
 
-if len(sys.argv) == 2 and os.path.isdir(sys.argv[1]):
-    # TODO: check y or n after input
-    Scanning(sys.argv[1])
+def showCompressCount():
     if tinify.compression_count is not None:
         print 'You have already used', tinify.compression_count, 'number of compressions this month.'
     else:
         print '**Warning:**\n   Nothing to compress in directory:', sys.argv[1]
+
+
+def confirmCompression(path):
+    input = raw_input('Are you sure to compress all images under:\n  {}\n(y or n ?): '.format(path))
+    if input != 'y':
+        print 'Compression aborted!'
+        sys.exit()
+
+
+if len(sys.argv) == 2 and os.path.isdir(sys.argv[1]):
+    confirmCompression(sys.argv[1])
+    initializeTinifyKey()
+    scanning(sys.argv[1])
+    showCompressCount()
 else:
     print 'Usage: python', sys.argv[0], '<target_directory>'
 
 
+# TODO: resizing width and height?
