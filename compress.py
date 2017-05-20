f08918bc921d25b68ef9c7f1e25142deaa49752c
Waterstrong <sqlin@thoughtworks.com>
2017-05-20 11:25:15 +0800

    match more type of images and enable recursively scanning

--- compress.py
+++ compress.py
@@ -3,35 +3,37 @@ import sys
 import re
 import tinify
 
-# TODO: support more image types?
-image_pattern = re.compile('.*\.(jpg$|png$)')
+IMAGE_PATTERN = re.compile('.*\.(jpg$|jpeg$|png$)', re.I)
 
-def initializeTinifyKey():
+def load_tinify_key(key_file):
     print 'Initializing tinify key...'
-    tinify_key_file = open('tinify.key', 'r')
+    tinify_key_file = open(key_file, 'r')
     tinify.key = tinify_key_file.read()
     tinify_key_file.close()
 
 
-def scanning(rootDir):
-    for file in os.listdir(rootDir):
-        path = os.path.join(rootDir, file)
+def compress_images(root_dir, recursively):
+    for file in os.listdir(root_dir):
+        path = os.path.join(root_dir, file)
         if os.path.isdir(path):
-            scanning(path)
-        elif image_pattern.match(path):
+            if recursively:
+                compress_images(path, recursively)
+            else:
+                print 'Subdirectory scanning ignored:', path
+        elif IMAGE_PATTERN.match(path):
             print 'Start compressing image:', path
-            tinify.from_file(path).to_file(path)
+            # tinify.from_file(path).to_file(path)
             print 'Compression done!\n'
 
 
-def showCompressCount():
+def show_compressed_count():
     if tinify.compression_count is not None:
         print 'You have already used', tinify.compression_count, 'number of compressions this month.'
     else:
-        print '**Warning:**\n   Nothing to compress in directory:', sys.argv[1]
+        print '**Warning:**\n   Nothing to compress in the directory.'
 
 
-def confirmCompression(path):
+def confirm_compression(path):
     input = raw_input('Are you sure to compress all images under:\n  {}\n(y or n ?): '.format(path))
     if input != 'y':
         print 'Compression aborted!'
@@ -39,10 +41,10 @@ def confirmCompression(path):
 
 
 if len(sys.argv) == 2 and os.path.isdir(sys.argv[1]):
-    confirmCompression(sys.argv[1])
-    initializeTinifyKey()
-    scanning(sys.argv[1])
-    showCompressCount()
+    confirm_compression(sys.argv[1])
+    load_tinify_key('tinify.key')
+    compress_images(sys.argv[1], True)
+    show_compressed_count()
 else:
     print 'Usage: python', sys.argv[0], '<target_directory>'
 
