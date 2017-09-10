762447283233e974e33f25935e957d9f3cac0b6a
Waterstrong <sqlin@thoughtworks.com>
2017-09-10 11:59:54 +0800

    enhance the auto upgrade

--- tinypng.sh
+++ tinypng.sh
@@ -2,5 +2,6 @@
 
 argv=$1
 current_dir=$(dirname "$(readlink "$0")")
-git -C "${current_dir}" pull -f
+git -C "${current_dir}" reset --hard >/dev/null 2>&1 &
+git -C "${current_dir}" pull >/dev/null 2>&1 &
 python "${current_dir}/compress.py" ${argv}
