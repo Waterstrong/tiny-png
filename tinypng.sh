9e750da89ff652dd91b69bc378f8c5a910fcc7b7
Waterstrong <sqlin@thoughtworks.com>
2017-09-10 12:02:26 +0800

    add git pull rebase

--- tinypng.sh
+++ tinypng.sh
@@ -3,5 +3,5 @@
 argv=$1
 current_dir=$(dirname "$(readlink "$0")")
 git -C "${current_dir}" reset --hard >/dev/null 2>&1 &
-git -C "${current_dir}" pull >/dev/null 2>&1 &
+git -C "${current_dir}" pull -r >/dev/null 2>&1 &
 python "${current_dir}/compress.py" ${argv}
